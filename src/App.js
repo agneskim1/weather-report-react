644e47f99ab7bccb9fdee7dce891d3ff29ee31ac
Agnes Kim <agneskim4@gmail.com>
2023-06-27 15:15:10 -0400

    add axios get request to find location

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
 import React from 'react';
 import SearchBar from './components/SearchBar';
+import axios from 'axios'
 
 function App() {
   const [location, setLocation] = React.useState("")
 
   const updateLocation = (location) => {
     setLocation(location)
+    const key = process.env.REACT_APP_LOCATION_KEY
+    console.log(key)
+    findLatitudeAndLongitude(location)
   }
+
+  const findLatitudeAndLongitude = async (city) => {
+    let latitude, longitude;
+    try {
+        const response = await axios.get('https://us1.locationiq.com/v1/search.php',
+        {
+            params: {
+                q: city,
+                key: process.env.LOCATION_KEY,
+                format: "json"
+            }
+        });
+        latitude = response.data[0].lat;
+        longitude = response.data[0].lon;
+        console.log(latitude,longitude)
+    return { latitude, longitude };
+    } catch (err) {
+        console.log(err.message)
+    }}
   return (
     <div className="App">
       <header className="App-header">
@@ -17,7 +40,7 @@ function App() {
         <SearchBar locationCallBack = {updateLocation}/>
       </body>
     </div>
-  );
+  )
 }
 
 export default App;
